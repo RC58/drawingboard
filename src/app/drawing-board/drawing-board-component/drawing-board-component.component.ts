f692defc1a2edd3e7a0a292956ad1038ac9577db
Rohit Chaurasia <chaurasiarohit12@gmail.com>
2020-04-11 17:01:55 +0530

    added changes

--- src/app/drawing-board/drawing-board-component/drawing-board-component.component.ts
+++ src/app/drawing-board/drawing-board-component/drawing-board-component.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CommanService } from 'src/app/services/comman.service';
+import { ConstantPool } from '@angular/compiler';
 
 @Component({
   selector: 'app-drawing-board-component',
@@ -9,11 +10,15 @@ import { CommanService } from 'src/app/services/comman.service';
 export class DrawingBoardComponentComponent implements OnInit {
 
   @Input('strokColor') public globalColor;
+  drawType: string = 'path';
+  svgPath: string = 'http://www.w3.org/2000/svg';
+  undoPath = [];
 
   constructor(public _commanService: CommanService) { }
 
   ngOnInit() {
     let svgBoard: SVGSVGElement = <any>document.getElementById('svgBoard');
+    this.undoPath = [];
 
     //get co-ordinates of lines
     const svgPoint = (elem, x, y) => {
@@ -34,8 +39,7 @@ export class DrawingBoardComponentComponent implements OnInit {
 
     //call event when mouse down on pallet
     svgBoard.addEventListener('mousedown', (event) => {
-      const polyline = document.createElementNS('http://www.w3.org/2000/svg', 'polyline');
-      const start = svgPoint(svgBoard, event.clientX, event.clientY);
+      const path = document.createElementNS(this.svgPath, this.drawType);
       let points = [];
 
       //function to draw lines
@@ -44,11 +48,11 @@ export class DrawingBoardComponentComponent implements OnInit {
         let p = svgPoint(svgBoard, e.clientX, e.clientY);
         points.push(p.x.toString());
         points.push(p.y.toString());
-        polyline.setAttributeNS(null, 'points', points.toString());
-        polyline.setAttributeNS(null, "stroke", color);
-        polyline.setAttributeNS(null, 'stroke-width', '2');
-        polyline.setAttributeNS(null, 'fill', 'none');
-        svgBoard.appendChild(polyline);
+        path.setAttributeNS(null, 'd', 'M' + points.toString());
+        path.setAttributeNS(null, "stroke", color);
+        path.setAttributeNS(null, 'stroke-width', '2');
+        path.setAttributeNS(null, 'fill', 'none');
+        svgBoard.appendChild(path);
       }
 
       //function call when mouse up
@@ -69,7 +73,11 @@ export class DrawingBoardComponentComponent implements OnInit {
     //check user click on eraser
     if (this._commanService.eraserStatus) {
       let svgBoard: SVGSVGElement = <any>document.getElementById('svgBoard');
-      svgBoard.querySelectorAll('polyline').forEach(e => e.parentNode.removeChild(e));
+      svgBoard.querySelectorAll(this.drawType).forEach(e => {
+          e.parentNode.removeChild(e);
+          this.undoPath.push(e);
+        }
+      );
     }
   }
 
